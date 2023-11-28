ae7af3a5abc98c503032963f4a64c193924de306
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2023-11-28 01:00:55 +0000

    could be done

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import '../../styles/jumbotron.css'
+
 
 export default function Jumbotron({title, body}) {
   return (
-    <div><div class="jumbotron jumbotron-fluid">
-    <div class="container">
+    <div classname="d-flex justify-content-center" ><div className="jumbotron jumbotron-fluid mt-5 ">
+    <div className="container">
       <h1>{title}</h1>
       <p>{body}</p>
     </div>
--- src/js/component/navBar.jsx
+++ src/js/component/navBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">Start Bootstrap</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -15,13 +15,13 @@ const NavBar = () => {
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <a className="nav-link" href="#">About</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+          <a className="nav-link" href="#">Services</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a className="nav-link" href="#">Contact</a>
         </li>
       </ul>
     </div>
